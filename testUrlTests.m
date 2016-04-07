4b4c7f2bd25795ad73141f5095df4b4b0108a63e
shengbili <1320664343@qq.com>
2016-04-07 15:21:58 +0800

    添加的.plist文件

---
+++ testUrlTests.m
@@ -0,0 +1,40 @@
+//
+//  testUrlTests.m
+//  testUrlTests
+//
+//  Created by admin on 15/9/29.
+//  Copyright (c) 2015年 四川天籁文化传播有限公司. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface testUrlTests : XCTestCase
+
+@end
+
+@implementation testUrlTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
